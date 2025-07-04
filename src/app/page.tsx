b4dde15403c37cfaa64a97f5f5d45ef3f0f7e459
lennonconstantino <lennonconstantino@gmail.com>
2025-07-04 15:28:48 -0300

    create about component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import { getDataHome } from '@/utils/actions/get-data'
 import { HomeProps } from "@/utils/home.type";
 import { Hero } from '@/components/hero'
 import { Phone } from 'lucide-react'
+import { Services } from "@/components/home/services";
+import { Container } from '@/components/container'
 
 export default async function Home() {
   const { object }: HomeProps = await getDataHome();
-  console.log(object.title);
+  console.log(object);
 
   return (
     <main>
@@ -19,6 +21,11 @@ export default async function Home() {
         bannerUrl={object.metadata.banner.url}
         icon={<Phone size={24} color="#FFF" />}
       />
+
+      <Container>
+        <Services object={object} />
+      </Container>
+      
     </main>
   );
 }
