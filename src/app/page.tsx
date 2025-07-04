fbe9c7fcb57888fd71b74e62e73e44abdfe31e21
lennonconstantino <lennonconstantino@gmail.com>
2025-07-04 10:52:12 -0300

    apply styles on header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,7 @@
 export default function Home() {
   return (
     <main>
-      <div 
-      style={{ width: "100%", height: 600, backgroundColor: "#121212", marginBottom: 24}}
-      >
-        <h1>TESTE</h1>
-      </div>
 
-      <div 
-      style={{ width: "100%", height: 600, backgroundColor: "blue", marginBottom: 24}}
-      >
-        <h1>TESTE</h1>
-      </div>
-
-      <div 
-      style={{ width: "100%", height: 600, backgroundColor: "green", marginBottom: 24}}
-      >
-        <h1>TESTE</h1>
-      </div>
     </main>
   );
 }
